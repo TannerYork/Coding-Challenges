5cf5100158738024c34ed46936ada7f1360a2c36
Tanner York <tannerwyork@gmail.com>
2020-05-06 12:26:57 -0500

    Update LeetCode valid_anagram problem

--- LeetCode/ValidAnagram.py
+++ LeetCode/ValidAnagram.py
@@ -5,15 +5,14 @@
 ## Solution ##
 # The overall time complexity of this solution is O(n log n ) becase assuming n is the
 #  length of s, sorting s take O(n log n) time while comparing the two only takes O(n) time.
-# The space complexity of this solution is O(1) because the sorting and comparision is in-place.
 
 
 def isAnagram(s, t):
     if len(s) != len(t):
         return False
 
-    s.sort()
-    t.sort()
+    s = sorted(s)
+    t = sorted(t)
     for i in range(len(s)):
         if s[i] != t[i]:
             return False
